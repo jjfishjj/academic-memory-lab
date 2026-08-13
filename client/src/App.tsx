8a5fbe095765c29f3c1c3dfa3a486ee464f0bf94
jjfishjj <60953372+jjfishjj@users.noreply.github.com>
2026-08-13 14:30:30 +0800

    Restore original home route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,6 +14,7 @@ import PeriodicTutorial from "./pages/PeriodicTutorial";
 import AlkaliRoute from "./pages/AlkaliRoute";
 import HalogenRoute from "./pages/HalogenRoute";
 import ShadowEchoLab from "./pages/ShadowEchoLab";
+import PhoneticMagic from "./pages/PhoneticMagic";
 
 
 function Router() {
@@ -22,7 +23,7 @@ function Router() {
   return (
     <WouterRouter base={base || undefined}>
       <Switch>
-        <Route path={"/"} component={ShadowEchoLab} />
+        <Route path={"/"} component={PhoneticMagic} />
         <Route path={"/shadow-echo"} component={ShadowEchoLab} />
         <Route path={"/explore"} component={PeriodicTable} />
         <Route path={"/alkali"} component={AlkaliRoute} />
